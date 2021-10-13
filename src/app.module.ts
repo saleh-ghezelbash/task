1f5438eccdc02b6e504b91fa583b51559a500862
s.ghezelbash <s.ghezelbash>
2021-10-13 12:50:22 +0330

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { MONGO_CON, MONGO_CONNECTION } from './config/constants';
+import { MONGO_CON } from './config/constants';
 import { TaskModule } from './task/task.module';
 import { AuthModule } from './auth/auth.module';
 
 
 @Module({
-  imports: [MongooseModule.forRoot(MONGO_CONNECTION),TaskModule, AuthModule],
+  imports: [MongooseModule.forRoot(MONGO_CON),TaskModule, AuthModule],
   controllers: [],
   providers: [],
 })
